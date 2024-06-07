d0cdcc73342c5ed65525e9a80b995cd21d0ee564
Eduardo Coelho <coelho.edu96@gmail.com>
2024-06-07 20:56:34 -0300

    Created big-title, big-card and small-card components

---
+++ src/app/components/big-title/big-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-title',
+  templateUrl: './big-title.component.html',
+  styleUrls: ['./big-title.component.css']
+})
+export class BigTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/small-title/small-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-title',
+  templateUrl: './small-title.component.html',
+  styleUrls: ['./small-title.component.css']
+})
+export class SmallTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
