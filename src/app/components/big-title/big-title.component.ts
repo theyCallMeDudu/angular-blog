cf93424858d1f40b8cc5356613ea76f2af64b8f1
Eduardo Coelho <coelho.edu96@gmail.com>
2024-06-20 20:29:26 -0300

    Enabled dynamic properties to big-card and small-card components.

--- src/app/components/big-title/big-title.component.ts
+++ src/app/components/big-title/big-title.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-big-title',
   templateUrl: './big-title.component.html',
-  styleUrls: ['./big-title.component.css']
+  styleUrls: ['./big-title.component.css',
+              './big-title.responsive.component.css']
 })
 export class BigTitleComponent implements OnInit {
 
